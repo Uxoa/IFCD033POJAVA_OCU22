89a38de1b76318198ba4e529a1ada8282359107a
uxoa <palomita@gmail.com>
2024-06-14 14:20:33 +0200

    first commit

---
+++ StmpSocket.java
@@ -0,0 +1,15 @@
+//Crear un Cliente SMTP con Sockets con el lenguaje de programación Java.
+
+
+
+public class StmpSocket {
+
+    public static void main(String[] args){
+    
+    
+    
+    
+    }
+    
+    
+}
